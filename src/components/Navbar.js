d90c67fa702618befe124bfe7f20e6f5ebaed21e
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-09-11 17:15:16 +0200

    Change Navbar buttons style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
             <li className={styles.title}>Where's my money?!</li>
 
             <li><Link to="login">Login</Link></li>
-            <li><Link to="signup">Signup</Link></li>
+            <li><Link to="signup">Sign up</Link></li>
          </ul>
       </nav>
    );
