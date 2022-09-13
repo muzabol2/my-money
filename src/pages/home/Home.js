9c77bbe2eefc58840fcd356f55c7a54b3680bfb8
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-09-13 20:23:15 +0200

    132. Making a Transaction Form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,15 @@
 import styles from './Home.module.css';
+import TransactionForm from './TransactionForm';
 
 export default function Home() {
    return (
-      <div>
-         Home
+      <div className={styles.container}>
+         <div className={styles.content}>
+            Transaction list
+         </div>
+         <div className={styles.sidebar}>
+            <TransactionForm />
+         </div>
       </div>
    );
 }
