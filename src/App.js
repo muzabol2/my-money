e02bc2dbefc405779ffa1ac76c6aeab5cf96b07a
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-09-13 07:51:10 +0200

    130. Waiting Until Auth is Ready

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-         <Navbar/>
-         <Switch>
-            <Route exact path="/">
-               <Home />
-            </Route>
-            <Route path="/login">
-               <Login />
-            </Route>
-            <Route path="/signup">
-               <Signup />
-            </Route>
-         </Switch>
-      </BrowserRouter>
-    </div>
-  );
+   const { authIsReady } = useAuthContext();
+
+   return (
+      <div className="App">
+         {authIsReady && (
+            <BrowserRouter>
+               <Navbar />
+               <Switch>
+                  <Route exact path="/">
+                     <Home />
+                  </Route>
+                  <Route path="/login">
+                     <Login />
+                  </Route>
+                  <Route path="/signup">
+                     <Signup />
+                  </Route>
+               </Switch>
+            </BrowserRouter>
+         )}
+      </div>
+   );
 }
 
 export default App
