d13f016d3b5309c50641589c34ce96d84ae86545
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-09-14 08:21:19 +0200

    cleanup - tabs

--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -9,30 +9,32 @@ export default function TransactionForm() {
       console.log({name, amount});
    } 
 
-  return (
-    <>
-      <h3>Add a Transaction</h3>
-      <form onSubmit={handleSubmit}>
-         <label>
-            <span>Transaction name:</span>
-            <input 
-               type="text"
-               required
-               onChange={(e) => setName(e.target.value)}
-               value={name}
-            />
-         </label>
-         <label>
-            <span>Amount:</span>
-            <input 
-               type="number"
-               required
-               onChange={(e) => setAmount(e.target.value)}
-               value={amount}
-            />
-         </label>
-         <button>Add Transaction</button>
-      </form>
-    </>
-  )
+
+
+   return (
+      <>
+         <h3>Add a Transaction</h3>
+         <form onSubmit={handleSubmit}>
+            <label>
+               <span>Transaction name:</span>
+               <input
+                  type="text"
+                  required
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+               />
+            </label>
+            <label>
+               <span>Amount:</span>
+               <input
+                  type="number"
+                  required
+                  onChange={(e) => setAmount(e.target.value)}
+                  value={amount}
+               />
+            </label>
+            <button>Add Transaction</button>
+         </form>
+      </>
+   )
 }
