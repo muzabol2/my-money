5baa4150f042fce1e3686a954b935936218f222d
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-09-14 08:26:39 +0200

    136. Using the useFirestore Hook

--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -1,15 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useFirestore } from '../../hooks/useFirestore';
 
-export default function TransactionForm() {
+export default function TransactionForm({ uid }) {
    const [name, setName] = useState('');
    const [amount, setAmount] = useState('');
+   const { addDocument, response } = useFirestore('transactions');
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      console.log({name, amount});
-   } 
+      addDocument({ uid, name, amount });
+   }
 
-
+   useEffect(() => {
+      if (response.success) {
+         setName('');
+         setAmount('');
+      }
+   }, [response.success]);
 
    return (
       <>
