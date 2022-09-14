cf2a66be8f9e7c1abdc7b81932b49b0464ab326e
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-09-14 07:59:54 +0200

    134. Adding Firestore Documents

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,7 +10,27 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
    switch (action.type) {
-
+      case 'IS_PENDING':
+         return {
+            document: null,
+            isPending: true,
+            error: null,
+            success: null
+         };
+      case 'ADDED_DOCUMENT':
+         return {
+            document: action.payload,
+            isPending: false,
+            error: null,
+            success: true
+         };
+      case 'ERROR':
+         return {
+            document: null,
+            isPending: false,
+            error: action.payload,
+            success: false
+         };
       default:
          return state;
    }
@@ -22,11 +42,26 @@ export const useFirestore = (collection) => {
 
    const ref = projectFirestore.collection(collection);
 
-   const addDocument = (doc) => {
-
+   // only dispatch if not cancel
+   const dispatchIfNotCancelled = (action) => {
+      if (!isCancelled) {
+         dispatch(action);
+      }
    }
 
-   const deleteDocument = (id) => {
+   const addDocument = async (doc) => {
+      dispatch({ type: 'IS_PENDING' });
+
+      try {
+         const addedDocument = await ref.add(doc);
+         dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument });
+      }
+      catch (err) {
+         dispatchIfNotCancelled({ type: 'ERROR', payload: err.message });
+      }
+   }
+
+   const deleteDocument = async (id) => {
 
    }
 
