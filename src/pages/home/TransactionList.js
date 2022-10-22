df89fc9c2b46428355c88caf1cf68eb98ad1e14c
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-10-22 11:23:57 +0200

    feature: sum transactions, comma/dot

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -3,10 +3,16 @@ import './Home.css';
 
 export default function TransactionList({ transactions }) {
    const { deleteDocument } = useFirestore('transactions')
+   console.log("transactions", transactions);
+
+   const sum = transactions.reduce((accumulator, transaction) => {
+      return accumulator + Number(transaction.amount);
+    }, 0);
 
    return (
       <ul className="transactions">
-         {transactions.map((transaction) => (
+         <h2>Sum: {sum.toFixed(2)} PLN</h2>
+         {transactions.map(transaction => (
             <li key={transaction.id}>
                <p className="transactionName">{transaction.transactionName}</p>
                <p className="amount">{transaction.amount}</p>
--- src/pages/home/validateTransaction.js
+++ src/pages/home/validateTransaction.js
@@ -1,17 +1,19 @@
 import * as yup from 'yup';
 
-const amountRules = /^(\d{0,12})([\.]\d{0,2})?$/;
-// Only numbers, max 12 digits and 2 decimal places
+const regExp = {
+   transactionName: /^[\s\p{L}0-9]+$/u,
+   amount: /^(\d{0,12})([.,]\d{0,2})?$/,
+}
 
-export const transactionSchema = yup.object().shape({
+export const transactionSchema = yup.object({
    transactionName: yup
       .string()
       .min(3, "3-20 characters\n")
       .max(20, "3-20 characters\n")
-      .matches(/^[A-Za-z0-9 ]*$/, "Only letters, numbers and spaces")
+      .matches(regExp.transactionName, "Only letters, numbers and spaces")
       .required("Required"),
    amount: yup
       .string()
-      .matches(amountRules, "\u2022 Only numbers\n\u2022 max 12 digits\n\u2022 dot(.) not comma(,) separator\n\u2022 only 2 decimal places")
+      .matches(regExp.amount, "\u2022 Only numbers\n\u2022 max 12 digits\n\u2022 dot(.) or comma(,) separator\n\u2022 only 2 decimal places")
       .required("Required"),
 });
