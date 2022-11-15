7ca0dd526c69408221340bbb961edb0a212798b4
Bolek <109333068+muzabol2@users.noreply.github.com>
2022-11-15 16:01:18 +0100

    Create 2 env - dev & prod

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyBiM8t-sv42ysSHCAVK7yQXQOoFH5L8KDI",
-   authDomain: "mymoney-834af.firebaseapp.com",
-   projectId: "mymoney-834af",
-   storageBucket: "mymoney-834af.appspot.com",
-   messagingSenderId: "316496355466",
-   appId: "1:316496355466:web:4377d2b880ce0fd795cd73"
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Init firebase
