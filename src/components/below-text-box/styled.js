7408f0739d8019eec5d1664e53340e642a8f7c55
Bolesław Muzalewski <bolekbilly@gmail.com>
2023-11-23 06:35:42 +0100

    Styles fixes

--- src/components/below-text-box/styled.js
+++ src/components/below-text-box/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledBelowContainer = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 300px;
+  max-width: 350px;
   margin: 5px auto 30px;
   padding: 10px 50px 10px;
   border: 1px solid #ddd;
--- src/global-styles.js
+++ src/global-styles.js
@@ -8,5 +8,14 @@ export default createGlobalStyle`
     padding: 0;
     font-family: 'Poppins', sans-serif;
     font-size: 1.1em;
-  }   
+  }
+
+  ul, li, p, h1, h2, h3, h4 {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul {
+    list-style-type: none;
+  }
 `;
